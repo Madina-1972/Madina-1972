95e36907571207bd6d16e1decaf2862b8360ce9c
Madina-1972 <abduhakimovamadina110@gmail.com>
2025-02-25 18:34:38 +0500

    update dom_17_4060550.js

--- dom_17_4060550.js
+++ dom_17_4060550.js
@@ -1,2 +1,2 @@
 document.getElementById("sarlavha").innerHTML = "O‘ZBEKISTON RESPUBLIKASI PREZIDENTINING QARORI";
-document.getElementById("haqida").innerHTML 
+document.getElementById("haqida").innerHTML = "O‘ZBEKISTON RESPUBLIKASI YOSHLAR ISHLARI AGENTLIGI FAOLIYATINI TASHKIL ETISH TO‘G‘RISIDA"
