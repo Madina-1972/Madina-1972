dbc376db6f795ad9c25b3b255d734d0925f3a9dd
Madina-1972 <abduhakimovamadina110@gmail.com>
2025-02-26 19:46:19 +0500

    update dom_17_4060550.js

--- dom_17_4060550.js
+++ dom_17_4060550.js
@@ -1,14 +1,6 @@
 document.getElementById("sarlavha").innerHTML = "O‘ZBEKISTON RESPUBLIKASI PREZIDENTINING QARORI";
 document.getElementById("haqida").innerHTML = "O‘ZBEKISTON RESPUBLIKASI YOSHLAR ISHLARI AGENTLIGI FAOLIYATINI TASHKIL ETISH TO‘G‘RISIDA";
-document.getElementById("aa1").innerHTML = "&#160; &#160; O‘zbekiston Respublikasi Prezidentining 2020-yil 30-iyundagi “O‘zbekiston Respublikasida yoshlarga oid davlat siyosatini tubdan isloh qilish va yangi bosqichga olib chiqish chora-tadbirlari to‘g‘risida”gi <a var class = function() {
-    
-};
-
-(function() {
-    this.prop = ""
-}).call(class.prototype);
-
-exports.class = class;href='#'>PF-6017-son<a> Farmoniga muvofiq:";
+document.getElementById("aa1").innerHTML = "&#160; &#160; O‘zbekiston Respublikasi Prezidentining 2020-yil 30-iyundagi “O‘zbekiston Respublikasida yoshlarga oid davlat siyosatini tubdan isloh qilish va yangi bosqichga olib chiqish chora-tadbirlari to‘g‘risida”gi <a style=' href='#'>PF-6017-son<a> Farmoniga muvofiq:";
 
 
 
