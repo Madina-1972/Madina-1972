fc9563abaf0b1387536c65b52ab36e82a3ff10cc
Madina-1972 <abduhakimovamadina110@gmail.com>
2025-03-02 21:07:14 +0500

    update 17_4060550.js

--- 17_4060550.js
+++ 17_4060550.js
@@ -1,6 +1,6 @@
 document.getElementById("sarlavha").innerHTML = "O‘ZBEKISTON RESPUBLIKASI PREZIDENTINING QARORI";
 document.getElementById("haqida").innerHTML = "O‘ZBEKISTON RESPUBLIKASI YOSHLAR ISHLARI AGENTLIGI FAOLIYATINI TASHKIL ETISH TO‘G‘RISIDA";
-document.getElementById("aa1").innerHTML = "&#160; &#160; O‘zbekiston Respublikasi Prezidentining 2020-yil 30-iyundagi “O‘zbekiston Respublikasida yoshlarga oid davlat siyosatini tubdan isloh qilish va yangi bosqichga olib chiqish chora-tadbirlari to‘g‘risida”gi <a style='color:#0067a5;' href='#'>PF-6017-son<a> farmoniga muvofiq: <br> &#160; &#160; 1. Quyidagilar:  <br> &#160; &#160; a) O‘zbekiston Respublikasi Yoshlar ishlari agentligining tashkiliy tuzilmasi 1-ilovaga muvofiq:  <br> &#160; &#160; b) O‘zbekiston Respublikasi Davlat budjetidan moliyalashtiriladigan boshqaruv xodimlarining umumiy cheklangan soni:;
+document.getElementById("aa1").innerHTML = "&#160; &#160; O‘zbekiston Respublikasi Prezidentining 2020-yil 30-iyundagi “O‘zbekiston Respublikasida yoshlarga oid davlat siyosatini tubdan isloh qilish va yangi bosqichga olib chiqish chora-tadbirlari to‘g‘risida”gi <a style='color:#0067a5;' href='#'>PF-6017-son<a> farmoniga muvofiq: <br> &#160; &#160; 1. Quyidagilar:  <br> &#160; &#160; a) O‘zbekiston Respublikasi Yoshlar ishlari agentligining tashkiliy tuzilmasi 1-ilovaga muvofiq:  <br> &#160; &#160; b) O‘zbekiston Respublikasi Davlat budjetidan moliyalashtiriladigan boshqaruv xodimlarining umumiy cheklangan soni:";
 
 
 
